07a36cbd2f448b8c5010baabd18b7eb4d41126b1
AbnocChinwads <rnemo@hotmail.co.uk>
2020-03-17 12:54:31 +0000

    Reduced dark/light mode to only one button

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,12 @@
 $(document).ready(function () {
-    $("#btn-dark").on("click", function () {
-        $("body").addClass("body-dark");
-        $("nav").removeClass("navbar-light bg-light").addClass("navbar-dark bg-dark");
-        $("button").removeClass("btn-light").addClass("btn-dark");
-        $("footer").removeClass("footer").addClass("footer-dark");
-        $("a").removeClass("anchor-styling").addClass("anchor-styling-dark");
-    });
-
-    $("#btn-light").on("click", function () {
-        $("body").removeClass("body-dark");
-        $("nav").removeClass("navbar-dark bg-dark").addClass("navbar-light bg-light");
-        $("button").removeClass("btn-dark").addClass("btn-light");
-        $("footer").removeClass("footer-dark").addClass("footer");
-        $("a").removeClass("anchor-styling-dark").addClass("anchor-styling");
-    });
+    $("button").on("click", function () {
+        $("body").toggleClass("body-dark");
+        $("nav").toggleClass("navbar-dark bg-dark");
+        $(this).toggleClass("btn-dark");
+        $(this).text(function(i, text){
+          return text === "Light Mode" ? "Dark Mode" : "Light Mode";
+        })
+        $("footer").toggleClass("footer-dark");
+        $("a").toggleClass("anchor-styling-dark");
+    })
 });
